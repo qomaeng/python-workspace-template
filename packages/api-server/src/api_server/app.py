b1a8a32e5d13da3244e8e61bda78b2c0a2ae1d69
qomaeng <qomaeng@gmail.com>
2026-08-19 22:28:11 +0900

    fix: logging

--- packages/api-server/src/api_server/app.py
+++ packages/api-server/src/api_server/app.py
@@ -1,8 +1,13 @@
+from typing import Any
+
+from core.logging import setup_logger
 from fastapi import FastAPI
+
+setup_logger()
 
 app = FastAPI()
 
 
 @app.get("/")
-def root():
+def root() -> dict[str, Any]:
     return {"Hello": "World"}
--- packages/auth-service/src/auth_service/__main__.py
+++ packages/auth-service/src/auth_service/__main__.py
@@ -1,8 +1,15 @@
 import asyncio
+from logging import getLogger
+
+from core.logging import setup_logger
+
+setup_logger()
+
+logger = getLogger(__name__)
 
 
-async def main():
-    print("Hello World")
+async def main() -> None:
+    logger.info("Hello World")
 
 
 if __name__ == "__main__":
